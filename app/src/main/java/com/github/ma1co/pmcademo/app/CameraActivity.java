c423e520c6cffa97f196f1116be072b6806da2ea
Vojtěch Biberle <vojtech.biberle@gmail.com>
2026-06-11 13:04:28 +0200

    fix: reset camera immediately after capture

--- app/src/main/java/com/github/ma1co/pmcademo/app/CameraActivity.java
+++ app/src/main/java/com/github/ma1co/pmcademo/app/CameraActivity.java
@@ -1,7 +1,6 @@
 package com.github.ma1co.pmcademo.app;
 
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
@@ -10,14 +9,11 @@ import com.sony.scalar.hardware.CameraEx;
 import java.io.IOException;
 
 public class CameraActivity extends BaseActivity implements SurfaceHolder.Callback {
-    private static final int CAPTURE_COOLDOWN_MS = 5000;
-
     private SurfaceHolder surfaceHolder;
     private CameraEx camera;
-    private Handler handler;
     private boolean ready;
     private boolean capturing;
-    private boolean coolingDown;
+    private boolean resetting;
     private boolean shutterDown;
     private boolean enterDown;
     private int shotCount;
@@ -30,8 +26,7 @@ public class CameraActivity extends BaseActivity implements SurfaceHolder.Callba
         SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        handler = new Handler();
-        setStatus("HX90V Intervalometer 0.7\nwaiting for preview...");
+        setStatus("HX90V Intervalometer 0.8\nwaiting for preview...");
     }
 
     @Override
@@ -40,7 +35,7 @@ public class CameraActivity extends BaseActivity implements SurfaceHolder.Callba
         camera = CameraEx.open(0, null);
         ready = false;
         capturing = false;
-        coolingDown = false;
+        resetting = false;
         shutterDown = false;
         enterDown = false;
         shotCount = 0;
@@ -53,10 +48,9 @@ public class CameraActivity extends BaseActivity implements SurfaceHolder.Callba
         releaseCamera();
         ready = false;
         capturing = false;
-        coolingDown = false;
+        resetting = false;
         shutterDown = false;
         enterDown = false;
-        handler.removeCallbacksAndMessages(null);
         surfaceHolder.removeCallback(this);
     }
 
@@ -146,12 +140,11 @@ public class CameraActivity extends BaseActivity implements SurfaceHolder.Callba
     }
 
     private void takePicture() {
-        if (!ready || capturing || coolingDown || camera == null) {
-            setStatus("Not ready\nready=" + ready + " capturing=" + capturing + "\ncooldown=" + coolingDown);
+        if (!ready || capturing || resetting || camera == null) {
+            setStatus("Not ready\nready=" + ready + " capturing=" + capturing + "\nresetting=" + resetting);
             return;
         }
         capturing = true;
-        coolingDown = false;
         shotCount++;
         setStatus("Taking picture " + shotCount + "...");
         camera.startSelfTimerShutter();
@@ -162,7 +155,7 @@ public class CameraActivity extends BaseActivity implements SurfaceHolder.Callba
             @Override
             public void onError(int error, CameraEx camera) {
                 capturing = false;
-                coolingDown = false;
+                resetting = false;
                 setStatus("CameraEx error " + error + "\nS2/ENTER: retry");
             }
         });
@@ -181,35 +174,25 @@ public class CameraActivity extends BaseActivity implements SurfaceHolder.Callba
             @Override
             public void onEnd(int status, int reason, CameraEx camera) {
                 capturing = false;
-                beginCooldown("Capture ended status=" + status + " reason=" + reason);
+                resetCameraSession();
             }
         });
         camera.setStoreImageCompleteListener(new CameraEx.StoreImageCompleteListener() {
             @Override
             public void onDone(int status, CameraEx.StoreImageInfo info, CameraEx camera) {
                 capturing = false;
-                beginCooldown("Store complete status=" + status);
+                resetCameraSession();
             }
         });
     }
 
-    private void beginCooldown(String message) {
-        if (coolingDown) {
+    private void resetCameraSession() {
+        if (resetting) {
             return;
         }
-        coolingDown = true;
-        setStatus(message + "\nProcessing...");
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                resetCameraSession();
-            }
-        }, CAPTURE_COOLDOWN_MS);
-    }
-
-    private void resetCameraSession() {
+        resetting = true;
         ready = false;
-        coolingDown = false;
+        setStatus("Resetting camera...");
         releaseCamera();
         try {
             camera = CameraEx.open(0, null);
@@ -228,14 +211,16 @@ public class CameraActivity extends BaseActivity implements SurfaceHolder.Callba
             camera.getNormalCamera().startPreview();
             ready = true;
             capturing = false;
-            coolingDown = false;
+            resetting = false;
             shutterDown = false;
             enterDown = false;
             registerCameraExListeners();
             setReadyStatus();
         } catch (IOException e) {
+            resetting = false;
             setStatus("Preview failed\n" + e.getClass().getSimpleName() + "\nMENU: exit");
         } catch (RuntimeException e) {
+            resetting = false;
             setStatus("Preview failed\n" + e.getClass().getSimpleName() + "\nMENU: exit");
         }
     }
